a45ce55b348c226e2db85413d7368f41833ad5d9
muzudho <muzudho1@gmail.com>
2025-05-26 00:03:01 +0900

    app.vue と画像。

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
+  <!--
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+      <NuxtRouteAnnouncer />
+      <NuxtWelcome />
+  </div>
+  -->
+  <div>
+    <h1>ワラベンチャー</h1>
+    Grok の助けを得て、ホームページ作成中（＾ｑ＾）<br/>
+    <img src="@/assets/img/202505__grok__25-warabenture-2.png" />
+    <img src="@/assets/img/202505__grok__25-warabenture-1.png" />
   </div>
 </template>
+
+<style>
+body{
+  background-color: pink;
+}
+</style>
