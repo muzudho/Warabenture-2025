b86e7b4e7119ab25f8c43fa58ea9508a9dd48722
muzudho <muzudho1@gmail.com>
2025-06-01 17:49:12 +0900

    journal スタイル

--- app.vue
+++ app.vue
@@ -6,22 +6,35 @@
   </div>
   -->
   <div>
-    <h1>ワラベンチャー</h1>
-    Grok の助けを得て、ホームページ作成中（＾ｑ＾）<br/>
-    <img src="@/assets/img/202505__grok__25-warabenture-2.png" />
-    <img src="@/assets/img/202505__grok__25-warabenture-1.png" /><br/>
-    <div class="talk">
-      <img src="@/assets/img/202101__character__31--ramen-tabero-futsu2.png" />
-      <span>文字は出せるかだぜ？</span>
+    <h1># ワラベンチャー</h1>
+
+    <h2>## [2025-06-01_Sun]</h2>
+    <div class="div2">
+      <div class="talk">
+        <img src="@/assets/img/202101__character__24-o2o2o3o0.png" />
+        <span>文字は出せるかだぜ？</span>
+      </div>
+      <div class="talk">
+        <img src="@/assets/img/202101__character__28--kifuwarabe-futsu-o2o0.png" />
+        <span>出せてるぜ。</span>
+      </div>
+      <div class="talk">
+        <img src="@/assets/img/202108__character__12--ohkina-hiyoko-futsu2-o2o0.png" />
+        <span>なんか画像がボヤけてない？<br/>サイズもマチマチだし。</span>
+      </div>
+      <div class="talk">
+        <img src="@/assets/img/202101__character__24-o2o2o3o0.png" />
+        <span>まあ、そこはゆくゆくは修正だな。</span>
+      </div>
     </div>
-    <div class="talk">
-      <img src="@/assets/img/202101__character__28--kifuwarabe-futsu.png" />
-      <span>出せてるぜ。</span>
+
+    <h2>## [2025-05-24_Sat]</h2>
+    <div class="div2">
+      Grok の助けを得て、ホームページ作成中（＾ｑ＾）<br/>
+      <img src="@/assets/img/202505__grok__25-warabenture-2.png" />
+      <img src="@/assets/img/202505__grok__25-warabenture-1.png" /><br/>
     </div>
-    <div class="talk">
-      <img src="@/assets/img/202108__character__12--ohkina-hiyoko-futsu2.png" />
-      <span>あ、<br/>い、<br/>う。</span>
-    </div>
+
   </div>
 </template>
 
@@ -40,8 +53,11 @@ h2 {
   color: #EEEEEE;
   margin-left: 60px;
   padding-left: 4px;
-  padding-top: 6px;
-  padding-bottom: 2px;
+}
+
+div.div2 {
+  color: #111111;
+  margin-left: 90px;
 }
 
 p.p2 {
