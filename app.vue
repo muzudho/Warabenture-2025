814d5898769b7a71ddc08596572d77f22591887b
muzudho <muzudho1@gmail.com>
2025-06-01 17:08:15 +0900

    文字の縦揃え。

--- app.vue
+++ app.vue
@@ -9,7 +9,19 @@
     <h1>ワラベンチャー</h1>
     Grok の助けを得て、ホームページ作成中（＾ｑ＾）<br/>
     <img src="@/assets/img/202505__grok__25-warabenture-2.png" />
-    <img src="@/assets/img/202505__grok__25-warabenture-1.png" />
+    <img src="@/assets/img/202505__grok__25-warabenture-1.png" /><br/>
+    <div class="talk">
+      <img src="@/assets/img/202101__character__31--ramen-tabero-futsu2.png" />
+      <span>文字は出せるかだぜ？</span>
+    </div>
+    <div class="talk">
+      <img src="@/assets/img/202101__character__28--kifuwarabe-futsu.png" />
+      <span>出せてるぜ。</span>
+    </div>
+    <div class="talk">
+      <img src="@/assets/img/202108__character__12--ohkina-hiyoko-futsu2.png" />
+      <span>あ、<br/>い、<br/>う。</span>
+    </div>
   </div>
 </template>
 
@@ -35,5 +47,10 @@ h2 {
 p.p2 {
   color: #111111;
   margin-left: 90px;
+}
+
+div.talk{
+  display: flex;
+  align-items: center;
 }
 </style>
