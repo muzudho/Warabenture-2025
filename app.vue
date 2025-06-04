55eb82515576ab3569e966b98df9a1881a94a252
muzudho <muzudho1@gmail.com>
2025-06-04 23:04:40 +0900

    ジャーナル、画像追加。

--- app.vue
+++ app.vue
@@ -11,6 +11,42 @@
       今さら、手作りのホームページ。（Created by むずでょ）
     </div>
 
+    <h2>## [2025-06-04_Wed]</h2>
+    <div class="div2">
+      <div class="talk">
+        <div class="face-container">
+          <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+        </div>
+        <div class="baloon-tail"></div>
+        <div class="baloon">
+          👇 今日は Grok に聞いて、コードのワーニングを掃除しただけだぜ。
+        </div>
+      </div>
+      <a target="_blank" href="/img/journal/202506/202506__program__04--2248-tauri-o2oA15o0.png">
+        <img class="scr-shot" src="/img/journal/202506/202506__program__04--2248-tauri-o2oA15o0.png">
+      </a><br/>
+      <a target="_blank" href="/img/journal/202506/202506__program__04--2249-tauri-o2oA16o0.png">
+        <img class="scr-shot" src="/img/journal/202506/202506__program__04--2249-tauri-o2oA16o0.png">
+      </a><br/>
+      <div class="talk">
+        <div class="face-container">
+          <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+        </div>
+        <div class="baloon-tail"></div>
+        <div class="baloon">
+          👆 変数名、関数名を<span class="code-b">snake_case</span>から<span class="code-b">camelCase</span>に変えたぜ。
+        </div>
+      </div>
+      <div class="talk">
+        <div class="face-container">
+          <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+        </div>
+        <div class="baloon-tail"></div>
+        <div class="baloon">
+          👆 Rust で作った関数の戻り値を、TypeScript 側できっちり受け取れるように修正したぜ。
+        </div>
+      </div>
+    </div>
 
     <h2>## [2025-06-03_Tue]</h2>
     <div class="div2">
