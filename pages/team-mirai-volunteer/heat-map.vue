6fe338966f695654bc4675c88f555b44fb70244a
muzudho <muzudho1@gmail.com>
2025-06-09 18:26:05 +0900

    Heat Map 2.0.0 説明追加。

--- pages/team-mirai-volunteer/heat-map.vue
+++ pages/team-mirai-volunteer/heat-map.vue
@@ -1,8 +1,98 @@
 <template>
-    <h1>ワラベンチャー</h1>
-    <h2>チームみらいボランティア</h2>
-    <h3>ヒートマップ</h3>
-    <div class="div3">
+    <NuxtLink to="/">ワラベンチャー</NuxtLink>　＞　<NuxtLink to="/team-mirai-volunteer">チームみらいボランティア</NuxtLink><br>
+    <h1>ヒートマップ</h1>
+    <ul>
+        <li><NuxtLink to="#functions">機能紹介</NuxtLink></li>
+        <li><NuxtLink to="#download">ダウンロードはこちらから</NuxtLink></li>
+        <li><NuxtLink to="#chatting">雑談</NuxtLink></li>
+    </ul>
+    <h2 id="functions">機能紹介</h2>
+    <div class="div2">
+        <a target="_blank" href="/img/journal/202506/202506__teamMirai__09--1610-heatMap16-o1o0.png">
+            <img class="scr-shot" src="/img/journal/202506/202506__teamMirai__09--1610-heatMap16-o1o0.png">
+        </a><br/>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                👆 ヒートマップを作れる。<br>
+                右上の凡例にあるように［以上］での比較だけ利用可能。
+            </div>
+        </div>
+        <a target="_blank" href="/img/journal/202506/202506__teamMirai__09--1730-heatMap23-o1o0.png">
+            <img class="scr-shot" src="/img/journal/202506/202506__teamMirai__09--1730-heatMap23-o1o0.png">
+        </a><br/>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                👆 もちろん、表示内容を替えれる。
+            </div>
+        </div>
+        <a target="_blank" href="/img/journal/202506/202506__teamMirai__09--1729-heatMap22-o1o0.png">
+            <img class="scr-shot" src="/img/journal/202506/202506__teamMirai__09--1729-heatMap22-o1o0.png">
+        </a><br/>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                👆 ごく短い文字数のメモにも使える。
+            </div>
+        </div>
+        <a target="_blank" href="/img/journal/202506/202506__teamMirai__09--1750-heatMap25-o1o0.png">
+            <img class="scr-shot" src="/img/journal/202506/202506__teamMirai__09--1750-heatMap25-o1o0.png">
+        </a><br/>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                👆 仕込みとして、<br>
+                この<span class="font-x2">地図</span>を描くとこは、罫線とセル結合を駆使して神エクセル、頑張ってくれだぜ。<br>
+                本来、東西南北に隣り合わせではないとか、市区町村のつながりがおかしくても、グリッドに押し込んで並べろだぜ。<br>
+                <br/>
+                正確さが損なわれる代わりに、その代償分ほどの取扱いのシンプルさの向上を得られる。<br>
+                トレードオフだぜ。
+            </div>
+        </div>
+        <a target="_blank" href="/img/journal/202506/202506__teamMirai__09--1619-heatMap16-o3o0.png">
+            <img class="scr-shot" src="/img/journal/202506/202506__teamMirai__09--1619-heatMap16-o3o0.png">
+        </a><br/>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                👆 地図と<span class="font-x2">データ</span>はワークシートを分けている。<br>
+                データは［e-Stat］の［国勢調査］とかから持ってきてくれだぜ。<br>
+                <a target="_blank" href="https://www.e-stat.go.jp/">https://www.e-stat.go.jp/</a>
+            </div>
+        </div>
+        <a target="_blank" href="/img/journal/202506/202506__teamMirai__09--1740-heatMap24-o1o0.png">
+            <img class="scr-shot" src="/img/journal/202506/202506__teamMirai__09--1740-heatMap24-o1o0.png">
+        </a><br/>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                👆 地図とデータを紐づける、いわゆる<span class="font-x2">制御</span>もワークシートが分かれているから、<br>
+                コピペで横展開しやすいように作ってある。<br>
+                使い方はワークブックの中に［手順書_XXXX］ワークシートを入れてあるから、それを読んでくれだぜ。
+            </div>
+        </div>
+    </div>
+    <h2 id="download">ダウンロードはこちらから</h2>
+    <div class="div2">
         <div class="talk">
             <div class="face-container">
                 <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
@@ -14,9 +104,6 @@
                 ダウンロード：<a href="/products/202506/Heat Map 2_0_0 - Saitama[20250609-1638].xlsm"><span class="font-x2">⏬</span>Heat Map 2_0_0 - Saitama[20250609-1638].xlsm</a>
             </div>
         </div>
-        <a target="_blank" href="/img/journal/202506/202506__teamMirai__09--1610-heatMap16-o1o0.png">
-            <img class="scr-shot" src="/img/journal/202506/202506__teamMirai__09--1610-heatMap16-o1o0.png">
-        </a><br/>
         <a target="_blank" href="/img/journal/202506/202506__teamMirai__09--1657-heatMap20-o1o0-mark.png">
             <img class="scr-shot" src="/img/journal/202506/202506__teamMirai__09--1657-heatMap20-o1o0-mark.png">
         </a><br/>
@@ -41,14 +128,23 @@
                 👆 ✅チェックしてブロックを外してくれだぜ。
             </div>
         </div>
+    </div>
+    <h2 id="chatting">雑談</h2>
+    <div class="div2">
         <div class="talk">
             <div class="face-container">
                 <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
             </div>
             <div class="baloon-tail"></div>
             <div class="baloon">
-                これはヒートマップを作る神エクセルだぜ。<br>
-                使い方はワークブックの中に［手順書_XXXX］ワークシートを入れてあるから、それを読んでくれだぜ。
+                神エクセルだぜ。<br>
+                <br>
+                ヒートマップって何で作るかというと、計画段階でも、末端の現場でも、どちらでも、そこでの判断を助けるためだぜ。<br>
+                <br>
+                そもそも生のデータってのは複雑だからな。<br>
+                正確さを損ねてでもやる大幅なシンプル化は、時間が有限の中で何かを決断しなければいけない瞬間に<br>
+                結局やることになる。<br>
+                ここまでシンプルにすると、ボードゲームみたいになる。
             </div>
         </div>
         <div class="talk">
@@ -77,7 +173,7 @@
             <div class="baloon-tail"></div>
             <div class="baloon">
                 作業効率化や、コミュニケーションのためのスマホを使ったナウいツールは公式の人が用意してくれるだろうから、<br>
-                <span class="font-x2">神エクセルが使いやすいんだ！</span>という人を補助するツールにしていくぜ。
+                <span class="font-x2">神エクセルで何でもできるんだ！</span>という人を補助するツールにしていくぜ。
             </div>
         </div>
         <div class="talk">
@@ -126,6 +222,45 @@
             <div class="baloon-tail"></div>
             <div class="baloon">
                 分かった分かった。
+            </div>
+        </div>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                最新の AI を提供する公式スタッフと、<br>
+                わたしの熟練した神エクセルでバトルな。
+            </div>
+        </div>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--2013-kifuwarabe-o1o1o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                しかしこんなツール、要らんのでは？<br>
+                お父んだけ使っていればいいのでは？
+            </div>
+        </div>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--2025-hiyoko-o1o1o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                案外、そんじょそこらに熟練度の上限に達してしまったヤドカリがいっぱいいて<br>
+                集まってくるかも知れないわよ？
+            </div>
+        </div>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--2013-kifuwarabe-o1o1o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                うわあ
             </div>
         </div>
     </div>
