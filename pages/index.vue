239637b8f373c135de9b845c62c37ac67a7687d7
muzudho <muzudho1@gmail.com>
2025-06-12 23:42:13 +0900

    ジャーナル追加。

--- pages/index.vue
+++ pages/index.vue
@@ -5,34 +5,97 @@
         <NuxtLink to="/team-mirai-volunteer">チームみらいボランティア</NuxtLink>のリンクはこちら。
     </div>
 
+    <h2>## [2025-06-12_Thu]</h2>
+    <div class="div2">
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--2013-kifuwarabe-o1o1o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                データをクリーニングするツールが欲しいぜ。
+            </div>
+        </div>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                全くだぜ。
+            </div>
+        </div>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--2013-kifuwarabe-o1o1o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                文字列を入力し、［埼玉県新座市］という文字列があれば［埼玉県,新座市］と分割するような、<br>
+                ４７都道府県に対応した変換アプリを作ってくれだぜ。
+            </div>
+        </div>
+        <a target="_blank" href="/img/journal/202506/202506__tauri__12--2156-csvCleaning-o1o0.png">
+            <img class="scr-shot" src="/img/journal/202506/202506__tauri__12--2156-csvCleaning-o1o0.png">
+        </a><br/>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                <span class="font-x2">👆</span> じゃあ 📄<span class="code-w">file-list-on-tauri</span>リポジトリーから自動生成ファイルを削除して、それ以外をコピーした、<br>
+                📄<span class="code-w">csv-cleaning-on-tauri</span>リポジトリーを作るぜ。
+            </div>
+        </div>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                <span class="code-b">git init</span>コマンドを使った操作は、[2025-06-03_Tue] の日記を読んでくれだぜ。
+            </div>
+        </div>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                今日は途中で終了。
+            </div>
+        </div>
+    </div>
+
     <h2>## [2025-06-05_Thu]</h2>
     <div class="div2">
         <div class="talk">
-        <div class="face-container">
-            <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
-        </div>
-        <div class="baloon-tail"></div>
-        <div class="baloon">
-            今日は、家の掃除をしたいぜ。
-        </div>
-        </div>
-        <div class="talk">
-        <div class="face-container">
-            <img src="@/assets/img/202506__character__01--2013-kifuwarabe-o1o1o0.png" />
-        </div>
-        <div class="baloon-tail"></div>
-        <div class="baloon">
-            じゃあ、風呂桶を掃除してくれだぜ。
-        </div>
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                今日は、家の掃除をしたいぜ。
+            </div>
         </div>
         <div class="talk">
-        <div class="face-container">
-            <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--2013-kifuwarabe-o1o1o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                じゃあ、風呂桶を掃除してくれだぜ。
+            </div>
         </div>
-        <div class="baloon-tail"></div>
-        <div class="baloon">
-            <span class="font-x2">✅</span> 風呂と洗面所、便器を拭き掃除したぜ。
-        </div>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                <span class="font-x2">✅</span> 風呂と洗面所、便器を拭き掃除したぜ。
+            </div>
         </div>
         <div class="talk">
         <div class="face-container">
@@ -210,16 +273,16 @@
     <h2>## [2025-06-04_Wed]</h2>
     <div class="div2">
         <div class="talk">
-        <div class="face-container">
-            <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
-        </div>
-        <div class="baloon-tail"></div>
-        <div class="baloon">
-            👇 今日は Grok に聞いて、コードのワーニングを掃除しただけだぜ。
-        </div>
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                👇 今日は Grok に聞いて、コードのワーニングを掃除しただけだぜ。
+            </div>
         </div>
         <a target="_blank" href="/img/journal/202506/202506__program__04--2248-tauri-o2oA15o0.png">
-        <img class="scr-shot" src="/img/journal/202506/202506__program__04--2248-tauri-o2oA15o0.png">
+            <img class="scr-shot" src="/img/journal/202506/202506__program__04--2248-tauri-o2oA15o0.png">
         </a><br/>
         <a target="_blank" href="/img/journal/202506/202506__program__04--2249-tauri-o2oA16o0.png">
         <img class="scr-shot" src="/img/journal/202506/202506__program__04--2249-tauri-o2oA16o0.png">
@@ -374,23 +437,23 @@
             ［ローカル・リポジトリー］との紐付け方法の説明が書いてあるぜ。
         </div>
         </div>
-        <div class="code-b">
-        git remote add origin https://github.com/muzudho/file-list-on-tauri.git
-        git branch -M main
-        git push -u origin main
-        </div><br>
         <a target="_blank" href="/img/journal/202506/202506__program__03--1146-tauri-o2o6o0-mark.png">
-        <img class="scr-shot" src="/img/journal/202506/202506__program__03--1146-tauri-o2o6o0-mark.png">
+            <img class="scr-shot" src="/img/journal/202506/202506__program__03--1146-tauri-o2o6o0-mark.png">
         </a><br/>
+        <div class="code-b">
+            git remote add origin https://github.com/muzudho/file-list-on-tauri.git<br>
+            git branch -M main<br>
+            git push -u origin main<br>
+        </div><br>
         <div class="talk">
-        <div class="face-container">
-            <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
-        </div>
-        <div class="baloon-tail"></div>
-        <div class="baloon">
-            👆 上記のコマンドを打鍵するぜ。［ローカル・リポジトリー］に［リモート・リポジトリー］の場所を教えて、<br>
-            ［ブランチ］を選び、そこへ［ローカル・リポジトリー］のファイルをプッシュしたぜ。
-        </div>
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                👆 上記のコマンドを打鍵するぜ。［ローカル・リポジトリー］に［リモート・リポジトリー］の場所を教えて、<br>
+                ［ブランチ］を選び、そこへ［ローカル・リポジトリー］のファイルをプッシュしたぜ。
+            </div>
         </div>
         <a target="_blank" href="/img/journal/202506/202506__program__03--1150-tauri-o2o7o0.png">
         <img class="scr-shot" src="/img/journal/202506/202506__program__03--1150-tauri-o2o7o0.png">
