448147c7e98f30f8cea725c52de83727d69361a7
muzudho <muzudho1@gmail.com>
2025-07-12 15:52:01 +0900

    ジャーナル追加。

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,101 @@
         <NuxtLink to="/team-mirai-volunteer">チームみらいボランティア</NuxtLink>のリンクはこちら。
     </div>
 
+    <h2>## [2025-07-12_Sat]</h2>
+    <div class="div2">
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                <span class="code-w">csv-cleaning-2-on-tauri</span>リポジトリーのパッケージ依存関係がおかしいらしい。<br/>
+                📄<span class="code-w">cargo.toml</span>ファイルから、<span class="code-w">api-all</span>フィーチャーを除去。
+            </div>
+        </div>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                👇 次に、<span class="code-w">cargo</span>を更新。
+            </div>
+        </div>
+        <pre class="code-b">
+cd src-tauri
+cargo update
+cargo clean
+pnpm install
+pnpm dev
+
+cd ..
+pnpm add @tauri-apps/cli@latest @tauri-apps/api@latest
+
+pnpm tauri info</pre><br/>
+        <pre class="code-b">
+&gt; notepad-on-tauri@0.1.0 tauri C:\Users\muzud\OneDrive\ドキュメント\GitHub\csv-cleaning-2-on-tauri
+&gt; tauri "info"
+
+
+[✔] Environment
+    - OS: Windows 10.0.26100 x86_64 (X64)
+    ✔ WebView2: 138.0.3351.77
+    ✔ MSVC:
+        - Visual Studio Build Tools 2022
+        - Visual Studio Community 2022
+    ✔ rustc: 1.87.0 (17067e9ac 2025-05-09)
+    ✔ cargo: 1.87.0 (99624be96 2025-05-06)
+    ✔ rustup: 1.28.2 (e4f3ad6f8 2025-04-28)
+    ✔ Rust toolchain: stable-x86_64-pc-windows-msvc (default)
+    - node: 22.16.0
+    - pnpm: 10.11.0
+    - npm: 10.8.2
+
+[-] Packages
+    - tauri 🦀: 2.6.2
+    - tauri-build 🦀: 2.3.0
+    - wry 🦀: 0.52.1
+    - tao 🦀: 0.34.0
+    - @tauri-apps/api : 2.6.0
+    - @tauri-apps/cli : 2.6.2
+
+[-] Plugins
+    - tauri-plugin-dialog 🦀: 2.3.0
+    - @tauri-apps/plugin-dialog : 2.2.2 (outdated, latest: 2.3.0)
+    - tauri-plugin-fs 🦀: 2.4.0
+    - @tauri-apps/plugin-fs : 2.3.0 (outdated, latest: 2.4.0)
+    - tauri-plugin-opener 🦀: 2.4.0
+    - @tauri-apps/plugin-opener : 2.2.7 (outdated, latest: 2.4.0)
+
+[-] App
+    - build-type: bundle
+    - CSP: unset
+    - frontendDist: ../dist
+    - devUrl: http://localhost:1420/
+    - framework: Vue.js
+    - bundler: Vite</pre><br/>
+        <pre class="code-b">
+rustup update</pre><br/>
+        <pre class="code-b">
+cd src-tauri
+cargo add tauri-plugin-fs
+pnpm add @tauri-apps/plugin-fs
+
+cd ..
+pnpm dev</pre><br/>
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01--1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                直らん。 📁<span class="code-w">node_modules</span> フォルダーを削除して、<span class="code-w">pnpm tauri dev</span>を打鍵。<br/>
+                コンパイル・エラーの数はだいぶ減った。
+            </div>
+        </div>
+    </div>
+
     <h2>## [2025-06-19_Thu]</h2>
     <div class="div2">
         <div class="talk">
