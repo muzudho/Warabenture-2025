8c556bfefa1e949a37f0f9e3f3d3dd5c6862e236
muzudho <muzudho1@gmail.com>
2025-07-25 17:45:35 +0900

    v-img タグを練習中。

--- pages/wara-city/map.vue
+++ pages/wara-city/map.vue
@@ -2,7 +2,7 @@
     <v-app>
         <v-main>
             あ。
-            <v-img src="/sea.png" width="32" height="32"></v-img>
+            <v-img src="/products/wara_city/img/tiles/sea.png" width="32" height="32"></v-img>
             ん。
         </v-main>
     </v-app>
