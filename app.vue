01df448ded14cf88e12c92b5e052c6d4d51beb8f
muzudho <muzudho1@gmail.com>
2025-08-08 19:05:05 +0900

    Navigation

--- app.vue
+++ app.vue
@@ -3,6 +3,20 @@
         <v-main>
             <NuxtLayout>
 
+                <!-- ナビゲーション -->
+                <nav>
+                    <!--
+                        pages/ フォルダー下のページのファイル名は、
+                        Nuxt 公式では kebab-case、
+                            📖 https://nuxt.com/docs/3.x/guide/directory-structure/pages
+                        Tauri では PascalCase が習慣的に使われています。異なるので注意。
+                    -->
+                    <router-link to="/">ホーム</router-link> |
+                    <router-link to="/about">About</router-link> |
+                    <router-link to="/page-layout">PageLayout</router-link>
+                </nav>
+
+                <!-- /pages フォルダーの中にある .vue ファイル がここに埋め込まれます。 -->
                 <NuxtPage />
 
             </NuxtLayout>
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>アバウト</h1>
+    <v-container>
+
+        <h1>About ページだぜ！</h1>
+
+        <p>制作者：むずでょ</p>
+        <p>ワラベンチャーでは、Ｗｅｂとデスクトップで両用できるアプリケーションを開発していっているぜ（＾▽＾）！</p>
+
+        <hr/>
+        <router-link to="/">ホームに戻る</router-link>
+
+    </v-container>
 </template>
