ef745dd0e27bc49ba8b272480ee5a010b818819e
muzudho <muzudho1@gmail.com>
2025-08-20 20:18:07 +0900

    互換性対応

---
+++ src/compatibles/compatible-style-value.ts
@@ -0,0 +1,18 @@
+//import type { Properties } from 'csstype';  // Nuxt3 用。使わない
+//import { StyleValue } from 'vue';   // Tauri2 用
+
+/*
+    👇 Tauri2 + Vue3 では以下の書き方ができる。
+
+        import { StyleValue } from 'vue';
+
+    👇 Nuxt 3 では、以下の書き方ができる。
+
+        import type { Properties } from 'csstype';
+        // これは使えない。 import { StyleValue } from 'vue';
+
+    これでは、デスクトップアプリ版と、ウェブアプリ版でコードに違いがでてきてしまう。
+    そこで、型名を直接コードに書かず、型の別名を使うようにする。
+ */
+//export type CompatibleStyleValue = StyleValue;  // Tauri2 用
+export type CompatibleStyleValue = Record<string, string | number>;     // Nuxt3 用
