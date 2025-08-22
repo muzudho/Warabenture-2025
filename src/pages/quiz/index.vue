c9a4305c063a16f5119af7e7f53559bd14b4367a
muzudho <muzudho1@gmail.com>
2025-08-22 23:33:55 +0900

    設定ボタン更新

--- src/pages/quiz/index.vue
+++ src/pages/quiz/index.vue
@@ -1,3 +1,42 @@
 <template>
-    
+    <the-grand-parent-header/>
+
+    <h2>クイズの章だぜ！</h2>
+    <section class="sec-2">
+
+        <div class="talk">
+            <div class="face-container">
+                <img src="@/assets/img/202506__character__01-1951-kifuwarabeNoOton-o1o2o0.png" />
+            </div>
+            <div class="baloon-tail"></div>
+            <div class="baloon">
+                <span class="font-x2">👇</span>ちょっと下のスライダーを左右に動かしてみてくれだぜ。
+            </div>
+        </div>
+    </section>
+
+    <the-grand-parent-footer/>
 </template>
+
+
+<script setup lang="ts">
+
+    // ##############
+    // # インポート #
+    // ##############
+
+    // ++++++++++++++++++
+    // + コンポーネント +
+    // ++++++++++++++++++
+
+    import TheGrandParentFooter from '../../the-footer.vue';
+    import TheGrandParentHeader from '../../the-header.vue';
+
+
+    // ++++++++++++++++++
+    // + コンポーザブル +
+    // ++++++++++++++++++
+
+    import { getPreviousLinkHighlight } from '../../composables/link-highlight';
+
+</script>
