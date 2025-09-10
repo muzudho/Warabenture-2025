49adec10f2ebd5da18ed02762a662d23a584100a
muzudho <muzudho1@gmail.com>
2025-09-10 13:24:49 +0900

    BlogArticle

---
+++ src/components/BlogArticle.vue
@@ -0,0 +1,36 @@
+<template>
+    <component
+        :is="getPageMap(props.page)"
+        v-if="getPageMap(props.page)"
+    />
+    <div
+        v-if="!getPageMap(props.page)"
+    >
+        ページが見つかりません。
+    </div>
+</template>
+
+<script setup lang="ts">
+
+    // ##############
+    // # インポート #
+    // ##############
+
+    // ++++++++++++++++++++++++++
+    // + インポート　＞　その他 +
+    // ++++++++++++++++++++++++++
+
+    import { getPageMap } from '@/pages/blog/page-map';
+
+
+    // ####################################
+    // # このコンポーネントが受け取る引数 #
+    // ####################################
+    
+    interface Props {
+        page: string;   // コンポーネント辞書のキー
+    }
+    // デフォルト値を設定
+    const props = defineProps<Props>();
+
+</script>
--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -7,93 +7,19 @@
         上の方の記事が新しいぜ（＾▽＾）！<br/>
     </section>
 
-    <!--
-        ページが見つかりませんの練習。
-        JavaScript では、 null は falsy。 !null （直接は書けないが）は true。
-    <div
-        v-if="!getPageMap('2025-09/32-wed')"
-    >
-        ページが見つかりませんの練習。
-    </div>
-    -->
-
-    🌟1: 2025-09/10-wed
-    <component
-        :is="getPageMap('2025-09/10-wed')"
-        v-if="getPageMap('2025-09/10-wed')"
-    />
-
-    🌟2: 2025-08/11-mon
-    <component
-        :is="getPageMap('2025-08/11-mon')"
-        v-if="getPageMap('2025-08/11-mon')"
-    />
-
-    🌟3: 2025-07/25-fri
-    <component
-        :is="getPageMap('2025-07/25-fri')"
-        v-if="getPageMap('2025-07/25-fri')"
-    />
-
-    🌟4: 2025-07/12-sat
-    <component
-        :is="getPageMap('2025-07/12-sat')"
-        v-if="getPageMap('2025-07/12-sat')"
-    />
-
-    🌟5: 2025-06/19-thu
-    <component
-        :is="getPageMap('2025-06/19-thu')"
-        v-if="getPageMap('2025-06/19-thu')"
-    />
-
-    🌟6: 2025-06/15-sun
-    <component
-        :is="getPageMap('2025-06/15-sun')"
-        v-if="getPageMap('2025-06/15-sun')"
-    />
-
-    🌟7: 2025-06/12-thu
-    <component
-        :is="getPageMap('2025-06/12-thu')"
-        v-if="getPageMap('2025-06/12-thu')"
-    />
-
-    🌟8: 2025-06/05-thu
-    <component
-        :is="getPageMap('2025-06/05-thu')"
-        v-if="getPageMap('2025-06/05-thu')"
-    />
-
-    🌟9: 2025-06/04-wed
-    <component
-        :is="getPageMap('2025-06/04-wed')"
-        v-if="getPageMap('2025-06/04-wed')"
-    />
-
-    🌟10: 2025-06/03-tue
-    <component
-        :is="getPageMap('2025-06/03-tue')"
-        v-if="getPageMap('2025-06/03-tue')"
-    />
-
-    🌟11: 2025-06/02-mon
-    <component
-        :is="getPageMap('2025-06/02-mon')"
-        v-if="getPageMap('2025-06/02-mon')"
-    />
-
-    🌟12: 2025-06/01-sun
-    <component
-        :is="getPageMap('2025-06/01-sun')"
-        v-if="getPageMap('2025-06/01-sun')"
-    />
-
-    🌟13: 2025-05/24-sat
-    <component
-        :is="getPageMap('2025-05/24-sat')"
-        v-if="getPageMap('2025-05/24-sat')"
-    />
+    <blog-article page="2025-09/10-wed"/>
+    <blog-article page="2025-08/11-mon"/>
+    <blog-article page="2025-07/25-fri"/>
+    <blog-article page="2025-07/12-sat"/>
+    <blog-article page="2025-06/19-thu"/>
+    <blog-article page="2025-06/15-sun"/>
+    <blog-article page="2025-06/12-thu"/>
+    <blog-article page="2025-06/05-thu"/>
+    <blog-article page="2025-06/04-wed"/>
+    <blog-article page="2025-06/03-tue"/>
+    <blog-article page="2025-06/02-mon"/>
+    <blog-article page="2025-06/01-sun"/>
+    <blog-article page="2025-05/24-sat"/>
 
     <the-parent-footer/>
 </template>
@@ -108,6 +34,7 @@
     // + インポート　＞　コンポーネント +
     // ++++++++++++++++++++++++++++++++++
 
+    import BlogArticle from '@/components/BlogArticle.vue';
     import TheGrandParentHeader from '../../the-header.vue';
     import TheParentFooter from '../the-footer.vue';
     import TheParentHeader from '../the-header.vue';
