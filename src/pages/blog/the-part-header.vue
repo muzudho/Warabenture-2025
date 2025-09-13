3baf29ba018a3875153bb5088a306c8580b4f42a
muzudho <muzudho1@gmail.com>
2025-09-13 15:32:36 +0900

    ページ構成変更

--- src/pages/blog/the-part-header.vue
+++ src/pages/blog/the-part-header.vue
@@ -1,21 +1,5 @@
 <template>
-    <the-app-header/>
-
     <section class="sec-0 mt-6 mb-6">
         上の方にある記事が新しいぜ（＾▽＾）！
     </section>
 </template>
-
-<script setup lang="ts">
-
-    // ##############
-    // # インポート #
-    // ##############
-
-    // ++++++++++++++++++++++++++
-    // + インポート　＞　ページ +
-    // ++++++++++++++++++++++++++
-
-    import TheAppHeader from '../the-app-header.vue';
-
-</script>
