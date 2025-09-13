98a54f513dd746e50b3940085fab2d0fdd562ed8
muzudho <muzudho1@gmail.com>
2025-09-13 15:22:11 +0900

    WIP: TOPへ戻るリンク用意中

--- src/pages/blog/the-part-body.vue
+++ src/pages/blog/the-part-body.vue
@@ -4,22 +4,53 @@
         router/page-map.ts, router/articles.json も編集してください（TODO: ここらへん自動生成化したい）
     -->
     <blog-article page="2025-09/13-sat"/>
-    <a href="#top">TOPに戻る</a>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-09/12-fri"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-09/11-thu"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-09/10-wed"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-08/11-mon"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-07/25-fri"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-07/12-sat"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-06/19-thu"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-06/15-sun"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-06/12-thu"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-06/05-thu"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-06/04-wed"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-06/03-tue"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-06/02-mon"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-06/01-sun"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
     <blog-article page="2025-05/24-sat"/>
+    <button-to-go-to-top class="sec-1 pt-6"/>
+
 </template>
 
 <script setup lang="ts">
@@ -33,5 +64,6 @@
     // ++++++++++++++++++++++++++++++++++
 
     import BlogArticle from '@/components/BlogArticle.vue';
+    import ButtonToGoToTop from '@/components/ButtonToGoToTop.vue';
 
 </script>
